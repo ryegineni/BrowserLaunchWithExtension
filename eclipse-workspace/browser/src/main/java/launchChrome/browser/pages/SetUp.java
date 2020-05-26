039475dec8eb4a29ea90af0d7e133fec0a8bfe28
ryegineni <ravikanth.yegineni@matic.network>
2020-05-26 14:58:55 +0530

    Automation extension

--- eclipse-workspace/browser/src/main/java/launchChrome/browser/pages/SetUp.java
+++ eclipse-workspace/browser/src/main/java/launchChrome/browser/pages/SetUp.java
@@ -63,7 +63,7 @@ public class SetUp {
 		//options.addArguments("--headless");
 		//options.addArguments("--window-size=1920,1080");
 		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
+		options.setExperimentalOption("useAutomationExtension", false);
 		Thread.sleep(3000);
 		options.addExtensions(new File(System.getProperty("user.dir") + "/extension_7_7_8_0.crx"));
 		DesiredCapabilities caps = DesiredCapabilities.chrome();
