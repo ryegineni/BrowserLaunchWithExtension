3783585e5959d229f99d8733344574d244802845
ryegineni <ravikanth.yegineni@matic.network>
2020-06-06 15:20:26 +0530

    update

--- eclipse-workspace/browser/src/main/java/launchChrome/browser/pages/SetUp.java
+++ eclipse-workspace/browser/src/main/java/launchChrome/browser/pages/SetUp.java
@@ -56,6 +56,8 @@ public class SetUp {
 	}
 
 	public void browserLaunch() throws InterruptedException {
+		try { configureDriverPath(); 
+		} catch (IOException e) { e.printStackTrace(); }
 		/*
 		 * try { configureDriverPath(); } catch (IOException e) { e.printStackTrace(); }
 		 * ChromeOptions options = new ChromeOptions();
