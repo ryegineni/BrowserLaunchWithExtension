dcf5899ef4237f0fcf799412acc6315efcad0382
ryegineni <ryegineni@gmail.com>
2020-06-14 02:44:58 +0530

    Update SetUp.java

--- eclipse-workspace/browser/src/main/java/launchChrome/browser/pages/SetUp.java
+++ eclipse-workspace/browser/src/main/java/launchChrome/browser/pages/SetUp.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Properties;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
